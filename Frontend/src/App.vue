d5991114d5f9bf2b4c90ea3f6a4c4ae6327f821c
Marco <marco-git@heavens-inc.de>
2025-01-07 12:57:23 +0100

    Adds VUE.js Hello World Frontend

---
+++ Frontend/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import HelloAxios from "./components/HelloAxios.vue"
+</script>
+
+<template>
+  <HelloAxios msg="Axios Hello World" />
+</template>
+
+<style scoped>
+</style>
---
+++ Frontend/src/components/HelloAxios.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+defineProps<{ msg: string }>()
+
+const backend = "api/"
+const message = ref( [{'id': -1, "name": "nullstring"}])
+
+onMounted(() =>{
+    axios
+    .get(backend + 'task')
+    .then((response) => {message.value = response.data})
+    }
+)
+</script>
+
+<template>
+    <h1>{{ msg }}</h1>
+    <div v-for="title in message">{{title.id}} => {{ title.name }}</div>
+</template>
